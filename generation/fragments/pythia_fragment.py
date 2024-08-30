1ce48e226bcd7daea0be66a58bf4d4f7cf7e40f5
Iason Krommydas <iason.krom@gmail.com>
2024-08-30 12:19:42 -0500

    some changes

--- generation/fragments/pythia_fragment.py
+++ generation/fragments/pythia_fragment.py
@@ -18,25 +18,12 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
     pythiaHepMCVerbosity = cms.untracked.bool(False),
     comEnergy = cms.double(13600.),
     PythiaParameters = cms.PSet(
-        pythia8CommonSettingsBlock,
-        pythia8CP5SettingsBlock,
-        processParameters = cms.vstring(
-            'JetMatching:setMad = off',
-            'JetMatching:scheme = 1',
-            'JetMatching:merge = on',
-            'JetMatching:jetAlgorithm = 2',
-            'JetMatching:etaJetMax = 5.',
-            'JetMatching:coneRadius = 1.',
-            'JetMatching:slowJetPower = 1',
-            'JetMatching:qCut = 30.', #this is the actual merging scale
-            'JetMatching:nQmatch = 5', #5 for 5-flavour scheme (matching of b-quarks)
-            'JetMatching:nJetMax = 1', #number of partons in born matrix element for highest multiplicity
-            'JetMatching:doShowerKt = off', #off for MLM matching, turn on for shower-kT matching
-        ),
-        parameterSets = cms.vstring('pythia8CommonSettings',
-                                    'pythia8CP5Settings',
-                                    'processParameters',
-                                    )
-    )
+            pythia8_example03 = cms.vstring('PartonLevel:ISR=off',
+                'PartonLevel:FSR = off',
+                'PartonLevel:MPI = off',
+                'HadronLevel:all = off',
+                'BeamRemnants:primordialKT = off'),
+            parameterSets = cms.vstring('pythia8_example03')
+        )
 )
 
